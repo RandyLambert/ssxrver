255540781796bf8725ac40b746556de90a06529c
RandyLambert <1249604462@qq.com>
2020-03-14 15:36:34 +0800

    add README.md

--- ssxrver/net/TcpConnection.h
+++ ssxrver/net/TcpConnection.h
@@ -3,7 +3,7 @@
 #include <memory>
 #include <boost/any.hpp>
 #include <netinet/in.h>
-#include "functional"
+#include <functional>
 #include "Buffer.h"
 #include "../base/noncopyable.h"
 namespace ssxrver
@@ -14,13 +14,14 @@ class EventLoop;
 class Channel;
 
 class TcpConnection : noncopyable,
-                     public std::enable_shared_from_this<TcpConnection>
+                      public std::enable_shared_from_this<TcpConnection>
 {
 public:
     typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
     typedef std::function<void(const TcpConnectionPtr &)> CloseCallback;
     typedef std::function<void(const TcpConnectionPtr &,
-                               Buffer *)> MessageCallback;
+                               Buffer *)>
+        MessageCallback;
     typedef std::function<void(const TcpConnectionPtr &)> ConnectionCallback;
     typedef std::function<void(const TcpConnectionPtr &)> WriteCompleteCallback;
     TcpConnection(EventLoop *loop,
@@ -29,19 +30,19 @@ public:
 
     EventLoop *getLoop() const { return loop_; }
 
-    bool connected() const {return state_ == kConnected; }
+    bool connected() const { return state_ == kConnected; }
     void send(string &&message); // C++11
     void send(const void *message, int len);
     void send(const char *message);
-    void send(const string& message);
+    void send(const string &message);
     void send(Buffer &&message); // C++11
     void send(Buffer *message);  // this one will swap data
-    void shutdown();             
+    void shutdown();
     void forceClose();
     void setTcpNoDelay(bool on);
     void startRead();
     void stopRead();
-    bool isReading() const {return reading_; }
+    bool isReading() const { return reading_; }
 
     void setContext(const boost::any &context) //把一个未知类型赋值
     {
@@ -57,17 +58,18 @@ public:
     {
         return &context_;
     }
-    void setConnectionCallback(const ConnectionCallback &cb){connectionCallback_ = cb;}
-    void setMessageCallback(const MessageCallback &cb){ messageCallback_ = cb;}
-    void setWriteCompleteCallback(const WriteCompleteCallback &cb){ writeCompleteCallback_ = cb;}
+    void setConnectionCallback(const ConnectionCallback &cb) { connectionCallback_ = cb; }
+    void setMessageCallback(const MessageCallback &cb) { messageCallback_ = cb; }
+    void setWriteCompleteCallback(const WriteCompleteCallback &cb) { writeCompleteCallback_ = cb; }
     int returnSockfd() const { return sockfd_; }
 
-    Buffer *inputBuffer(){ return &inputBuffer_;}
-    Buffer *outputBuffer(){ return &outputBuffer_;}
-    void setCloseCallback(const CloseCallback &cb){ closeCallback_ = cb; }
+    Buffer *inputBuffer() { return &inputBuffer_; }
+    Buffer *outputBuffer() { return &outputBuffer_; }
+    void setCloseCallback(const CloseCallback &cb) { closeCallback_ = cb; }
 
-    void connectEstablished(); 
-    void connectDestroyed(); 
+    void connectEstablished();
+    void connectDestroyed();
+
 private:
     enum StateE //连接的状态
     {
@@ -81,37 +83,36 @@ private:
     void handleClose();
     void handleError();
 
-    void sendInLoop(const string& message);
-    void sendInLoop(const char* message);
+    void sendInLoop(const string &message);
+    void sendInLoop(const char *message);
     void sendInLoop(const void *message, size_t len);
     void shutdownInLoop();
     void forceCloseInLoop();
 
-    void setState(StateE s){ state_ = s; }
+    void setState(StateE s) { state_ = s; }
     void startReadInLoop();
     void stopReadInLoop();
 
-    EventLoop *loop_;        //所属eventloop
-    StateE state_;           //FIXME atomic
+    EventLoop *loop_; //所属eventloop
+    StateE state_;    //FIXME atomic
     int sockfd_;
     std::unique_ptr<Channel> channel_;
     ConnectionCallback connectionCallback_;
     MessageCallback messageCallback_;
     WriteCompleteCallback writeCompleteCallback_;
     CloseCallback closeCallback_;
-    
-    Buffer inputBuffer_;   //应用层的接收缓冲区
-    Buffer outputBuffer_;  //应用层的发送缓冲区，当outputbuffer高到一定程度
-    boost::any context_;     //提供一个借口绑定位置类型的上线文对象
-    bool reading_;
 
+    Buffer inputBuffer_;  //应用层的接收缓冲区
+    Buffer outputBuffer_; //应用层的发送缓冲区，当outputbuffer高到一定程度
+    boost::any context_;  //提供一个借口绑定位置类型的上线文对象
+    bool reading_;
 };
 
 typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 void defaultMessageCallback(const TcpConnectionPtr &conn,
                             Buffer *buffer);
 void defaultConnectionCallback(const TcpConnectionPtr &conn);
-}
-}
+} // namespace net
+} // namespace ssxrver
 
 #endif
--- ssxrver/net/TcpServer.h
+++ ssxrver/net/TcpServer.h
@@ -20,23 +20,23 @@ public:
     typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
     typedef std::function<void(const TcpConnectionPtr &)> CloseCallback;
     typedef std::function<void(const TcpConnectionPtr &,
-                               Buffer *buffer)> MessageCallback;
+                               Buffer *buffer)>
+        MessageCallback;
     typedef std::function<void(const TcpConnectionPtr &)> ConnectionCallback;
     typedef std::function<void(const TcpConnectionPtr &)> WriteCompleteCallback;
     TcpServer(EventLoop *loop,
               struct sockaddr_in listenAddr);
     ~TcpServer();
 
-    EventLoop *getLoop() const {return loop_;}
+    EventLoop *getLoop() const { return loop_; }
     void setThreadNum(int numThreads);
     std::shared_ptr<EventLoopThreadPool> threadPool() { return threadPool_; }
 
     void start(); //启动线程池
-    void setConnectionCallback(const ConnectionCallback cb){ connectionCallback_ = std::move(cb);}
-    void setMessageCallback(MessageCallback cb){messageCallback_ = std::move(cb);}
-    void setWriteCompleteCallback(WriteCompleteCallback cb){ writeCompleteCallback_ = std::move(cb); }
+    void setConnectionCallback(const ConnectionCallback cb) { connectionCallback_ = std::move(cb); }
+    void setMessageCallback(MessageCallback cb) { messageCallback_ = std::move(cb); }
+    void setWriteCompleteCallback(WriteCompleteCallback cb) { writeCompleteCallback_ = std::move(cb); }
     void acceptSockListen();
-    
 
 private:
     void newConnection(int socked);
@@ -44,7 +44,7 @@ private:
     void removeConnectionInLoop(const TcpConnectionPtr &conn);
     void acceptHandRead();
 
-    typedef std::map<int,TcpConnectionPtr> ConnectionMap;
+    typedef std::map<int, TcpConnectionPtr> ConnectionMap;
 
     EventLoop *loop_;
     std::shared_ptr<EventLoopThreadPool> threadPool_;
@@ -52,13 +52,13 @@ private:
     MessageCallback messageCallback_;
     WriteCompleteCallback writeCompleteCallback_;
     std::atomic<bool> started_;
-    int nextConnId_;//下一个连接id
+    int nextConnId_;            //下一个连接id
     ConnectionMap connections_; //连接累彪保留这个服务器上的所有连接
     int acceptfd_;
     int idleFd_;
     Channel acceptChannel_;
 };
 
-}
-}
+} // namespace net
+} // namespace ssxrver
 #endif
