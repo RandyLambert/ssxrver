3bd23a8c99007ed2209ae272945490754398d8c8
RandyLambert <1249604462@qq.com>
2020-05-12 17:30:05 +0800

    test

--- test/logtest.cpp
+++ test/logtest.cpp
@@ -5,7 +5,6 @@ using namespace ssxrver;
 int main()
 {
     LOG_INFO << "dasdsadsa" << 1 << "sdsdsa";
-    LOG_TRACE << "DSDASDSA";
     LOG_DEBUG << "DSADSADASDSA";
     LOG_WARN << "DSADSADSADS";
     LOG_ERROR << "DDSADSADSA";
